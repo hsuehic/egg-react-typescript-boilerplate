82fd4c3e772b95ab9991ce7594bcce9bfc0e349c
xiaowei.xue <xiaowei.xue@shopee.com>
2019-12-17 18:11:43 +0800

    feat: users

--- app/controller/admin.ts
+++ app/controller/admin.ts
@@ -1,10 +1,8 @@
 import { Controller } from 'egg';
 import { route } from 'egg-controller';
-import * as admin from 'firebase-admin';
 
 export default class HomeController extends Controller {
-  @route('/admin')
-  public async index() {
+  private async renderPageWithBasicInfo<T extends {}>(state: T) {
     const { ctx } = this;
     const locale = ctx.cookies.get('locale') || 'en-US';
     await ctx.render('admin.js', {
@@ -22,76 +20,12 @@ export default class HomeController extends Controller {
         picture:
           'https://lh3.googleusercontent.com/-wB9ogfMmX5Q/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rf5zzOT6jsaazrdC0UaywEByS9dig.CMID/s192-c/photo.jpg',
       },
-    });
-  }
-  @route({
-    url: '/api/users',
-    method: 'get',
-  })
-  public async getUsers() {
-    return this.listAllUsers();
-  }
-
-  @route({
-    url: '/api/users',
-    method: 'post',
-    validateMetaInfo: [
-      {
-        name: 'email',
-        rule: {
-          type: 'string',
-        },
-      },
-      {
-        name: 'password',
-        rule: {
-          type: 'string',
-        },
-      },
-    ],
-  })
-  public async createUser(
-    email: string,
-    password: string,
-    phoneNumber: string,
-    emailVerified?: boolean,
-    displayName?: string,
-    photoURL?: string
-  ) {
-    return await admin.auth().createUser({
-      uid: email,
-      email,
-      password,
-      phoneNumber,
-      emailVerified,
-      displayName,
-      photoURL,
+      ...state,
     });
   }
 
-  @route({
-    url: '/api/users/:uid',
-    method: 'patch',
-  })
-  public async updateUser(uid: string, data: Partial<admin.auth.UserRecord>) {
-    return await admin.auth().updateUser(uid, data);
-  }
-
-  @route({
-    url: '/api/users/:uid',
-    method: 'delete',
-  })
-  public async deleteUser(uid: string) {
-    return await admin.auth().deleteUser(uid);
-  }
-
-  public async listAllUsers(nextPageToken?: string) {
-    const listUsersResult = await admin.auth().listUsers(100, nextPageToken);
-    let users = [...listUsersResult.users];
-    if (listUsersResult.pageToken) {
-      const remainUsers = await this.listAllUsers(listUsersResult.pageToken);
-      users = users.concat(remainUsers);
-    }
-    return users;
+  @route('/admin')
+  public async index() {
+    await this.renderPageWithBasicInfo({});
   }
 }
--- typings/app/controller/index.d.ts
+++ typings/app/controller/index.d.ts
@@ -5,11 +5,13 @@ import 'egg';
 import ExportAdmin from '../../../app/controller/admin';
 import ExportAuth from '../../../app/controller/auth';
 import ExportHome from '../../../app/controller/home';
+import ExportUser from '../../../app/controller/user';
 
 declare module 'egg' {
   interface IController {
     admin: ExportAdmin;
     auth: ExportAuth;
     home: ExportHome;
+    user: ExportUser;
   }
 }
